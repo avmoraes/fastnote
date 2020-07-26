fa2314f8d2583020cbf262a4d51ccb3d71a0a70e
Alex Vaz de Moraes <alexnc122@gmail.com>
2020-07-25 20:19:25 -0400

    Add unit test to get saved login return a valid User;

--- app/src/test/java/br/com/alex/fastnote/login/ui/viewmodel/LoginViewModelTest.kt
+++ app/src/test/java/br/com/alex/fastnote/login/ui/viewmodel/LoginViewModelTest.kt
@@ -7,11 +7,19 @@ import br.com.alex.fastnote.login.data.repository.LoginRepository
 import br.com.alex.fastnote.login.ui.view.LoginFormState
 import br.com.alex.fastnote.login.ui.view.LoginResultView
 import com.nhaarman.mockitokotlin2.mock
-import org.junit.Assert
-import org.junit.Before
-import org.junit.Rule
-import org.junit.Test
+import com.nhaarman.mockitokotlin2.times
+import com.nhaarman.mockitokotlin2.verify
+import com.nhaarman.mockitokotlin2.whenever
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.test.TestCoroutineDispatcher
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.runBlockingTest
+import kotlinx.coroutines.test.setMain
+import org.junit.*
+import org.junit.Assert.assertEquals
 import org.junit.runner.RunWith
+import org.mockito.ArgumentCaptor
+import org.mockito.Captor
 import org.mockito.Mock
 import org.mockito.junit.MockitoJUnitRunner
 
@@ -20,6 +28,8 @@ class LoginViewModelTest {
 
     @get:Rule
     val rule = InstantTaskExecutorRule()
+
+    private val testDispatcher = TestCoroutineDispatcher()
 
     private lateinit var viewModel: LoginViewModel
 
@@ -31,14 +41,25 @@ class LoginViewModelTest {
     private val loginFormStateObserver: Observer<LoginFormState> = mock()
     private val savedLoginObserver: Observer<Pair<Boolean, User>> = mock()
 
+    @Captor
+    lateinit var userInfoCaptor: ArgumentCaptor<Pair<Boolean, User>>
+
     @Before
     fun setup(){
+        Dispatchers.setMain(testDispatcher)
+
         viewModel = LoginViewModel(repository)
 
         viewModel.loginResult.observeForever(loginResultObserver)
         viewModel.loading.observeForever(loadingObserver)
         viewModel.loginFormState.observeForever(loginFormStateObserver)
         viewModel.savedLogin.observeForever(savedLoginObserver)
+    }
+
+    @After
+    fun tearDown() {
+        Dispatchers.resetMain() // reset main dispatcher to the original Main dispatcher
+        testDispatcher.cleanupTestCoroutines()
     }
 
     @Test
@@ -48,4 +69,24 @@ class LoginViewModelTest {
         Assert.assertNotNull(viewModel.loginFormState)
         Assert.assertNotNull(viewModel.savedLogin)
     }
+
+    @Test
+    fun `When view model getSavedLoginIfSaved get user then set user`() = runBlockingTest {
+        // Arrange
+        val user = User("fastnotetest@email.com", "123456")
+        val result = Pair(true, user)
+
+        whenever(repository.isSavedLogin()).thenReturn(true)
+        whenever(repository.getSavedLogin()).thenReturn(user)
+
+        // Act
+        viewModel.getSavedLoginIfSaved()
+
+        // Assert
+        userInfoCaptor.run {
+            verify(savedLoginObserver, times(1)).onChanged(capture())
+            assertEquals(result, value)
+        }
+    }
+
 }
